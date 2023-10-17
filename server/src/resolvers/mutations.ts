90eac949d35a24e2e3b05d6fd028a7481cfa68a7
bluedognumber8 <petro.krespi@gmail.com>
2023-10-17 23:08:31 +0500

    resolver, schema structure

---
+++ server/src/resolvers/mutations.ts
@@ -0,0 +1,5 @@
+const mutations = {
+  addBook: () => {},
+};
+
+export default mutations;
---
+++ server/src/resolvers/queries.ts
@@ -0,0 +1,19 @@
+const queries = {
+  books: () => books,
+};
+
+const books = [
+  {
+    title: "The Awakedsnidssdnsdg!",
+
+    author: "Kate Chopin",
+  },
+
+  {
+    title: "City of Glassfds",
+
+    author: "Paul Auster",
+  },
+];
+
+export default queries;
