f3c6114eac7b435a5bbffa100ed8db7510c8d623
bluedognumber8 <petro.krespi@gmail.com>
2023-10-19 05:33:17 +0500

    added new-component json

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Wrap>
+      <p>asdasdasd</p>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  display: flex;
+  height: 50px;
+  background-color: black;
+`;
+
+export default Header;
