6ce6948849f06b5be2cb1b91cc60a4af568f37a5
bluedognumber8 <petro.krespi@gmail.com>
2023-10-23 08:06:18 +0500

    added storybook

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
-import styled from "styled-components";
-import { useApolloClient, gql } from "@apollo/client";
 
 function App() {
-
   return (
     <>
       <h1>Hello, Home page!</h1>
